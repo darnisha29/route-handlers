ed713ba53f2db8488a910992ef4d2860f71a2e6f
Darnisha Patel <darnisha2910@gmail.com>
2024-05-02 11:38:30 +0530

    final

---
+++ src/app/profile/api/route.ts
@@ -0,0 +1,7 @@
+import { type NextRequest } from "next/server";
+
+export async function GET(request:NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    console.log(requestHeaders.get("Authorization"))
+    return new Response("profile API data")
+}
